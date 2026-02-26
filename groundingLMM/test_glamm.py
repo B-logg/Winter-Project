fe8b508f8c6cdeef7e5ef6afd770a1f821292963
서보성 <sbosung0610@gmail.com>
2026-02-26 12:45:46 +0900

    어시스턴트에 종료토큰 들어가는거 제거

--- groundingLMM/test_glamm.py
+++ groundingLMM/test_glamm.py
@@ -81,7 +81,12 @@ class ForestEvalDataset(Dataset):
         conv = conversation_lib.conv_templates["llava_v1"].copy()
         conv.append_message(conv.roles[0], DEFAULT_IMAGE_TOKEN + "\n" + clean_q)
         conv.append_message(conv.roles[1], "Based on the analysis, the results are as follows: ") 
-        input_ids_gen = tokenizer_image_token(conv.get_prompt(), self.tokenizer, return_tensors='pt')
+        
+        # 프롬프트 문자열을 뽑아낸 뒤, 끝에 몰래 붙은 종료 토큰을 완전히 뜯어냄
+        prompt_str = conv.get_prompt()
+        prompt_str = prompt_str.replace("</s>", "").replace(self.tokenizer.eos_token, "")
+
+        input_ids_gen = tokenizer_image_token(prompt_str, self.tokenizer, return_tensors='pt')
         
         # GT Masks Load
         gt_masks = []
