4af4b133ac1fe4caa51d6a681464ddd51579882c
서보성 <sbosung0610@gmail.com>
2026-02-26 12:04:49 +0900

    반복 토큰 제한 2

--- groundingLMM/test_glamm.py
+++ groundingLMM/test_glamm.py
@@ -238,7 +238,7 @@ def main():
                 max_new_tokens=256, 
                 use_cache=True,
                 bad_words_ids=[[tokenizer.unk_token_id]],
-                no_repeat_ngram_size=3
+                no_repeat_ngram_size=2
                 )
             raw_text = tokenizer.decode(output_ids[0, input_ids.shape[1]:], skip_special_tokens=False)
 
